f96242dd7cbf5b20a42bb24c9bcc483ce7635e8c
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-07-06 23:51:31 -0500

    Refactor using routers

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,18 +2,9 @@ from snippets.permissions import IsOwnerOrReadOnly
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import permissions, renderers, viewsets
-from rest_framework.decorators import action, renderer_classes
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-  return Response({
-    'users': reverse('user-list', request=request, format=format),
-    'snippets': reverse('snippet-list', request=request, format=format)
-  })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
